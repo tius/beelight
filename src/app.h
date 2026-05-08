cb85046611648e02404512c1dba6a9dcf5ee0194
tius <coding@tius.org>
2026-05-09 01:53:01 +0200

    Update submodule URLs and refactor app.h for console integration and logging improvements

--- src/app.h
+++ src/app.h
@@ -3,6 +3,7 @@
 //  see LICENSE for terms
 
 #include "settings.h"
+#include "lite/console.h"
 #include "lite/serial_out.h"
 #include "lite/log.h"
 #include "lite/clock.h"
@@ -31,7 +32,7 @@ public:
         
         //  process serial input
         if ( auto c = serial_.read(); c >= 0 ) {
-            serial_.write( char(c) );
+            console_.process( char(c) ) ;
         }
     }
 
@@ -43,8 +44,10 @@ private:
     lite::SerialOut     serial_out_{serial_};
     lite::StdOut        std_out_{serial_out_};
     AppLogger           logger_{serial_out_};
+    lite::Shell         shell_{};
+    lite::Console       console_{shell_, serial_out_};
 
-    Timer               timer_{MSG_BIND(this, on_timer)};
+    // Timer               timer_{MSG_BIND(this, on_timer)};
 
     App() {
         serial_out_.println("\n--- app started ---");
@@ -54,15 +57,17 @@ private:
         auto ms = lite::now_ms();
         LOG_WARN("now_ms: %u ms", ms);
 
-        timer_.start_periodic(1s);
+        console_.ready();
+
+        // timer_.start_periodic(1s);
     }
 
     // prevent copying
     App(const App&) = delete;
     App& operator=(const App&) = delete;
 
-    void on_timer() {
-        LOG_INFO("timer fired");
-    }
+    // void on_timer() {
+    //     LOG_INFO("timer fired");
+    // }
 
 };
